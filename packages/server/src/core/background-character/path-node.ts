bdfbd14700ff09ac7eaaa7b68988b3c43ed31cb2
David Yahalomi <davidyaha@gmail.com>
2017-09-28 11:29:00 +0300

    lint

--- packages/server/src/core/background-character/path-node.ts
+++ packages/server/src/core/background-character/path-node.ts
@@ -6,22 +6,17 @@ export class PathNode {
   points: [PathNode];
 }
 
-
 const loadPath = () => {
-
   const pathsJson = require('../../settings/background-characters-path.json');
   const pathsMap: Map<string, any> = new Map();
 
   pathsJson.forEach(jsonPath => pathsMap.set(jsonPath.id, jsonPath));
-  for (let [id, pathNode] of pathsMap) {
+  for (const [id, pathNode] of pathsMap) {
     pathNode.points = pathNode.points.map(pointId => pathsMap.get(pointId));
   }
 
-  const pathGraph =  Array.from(pathsMap.values());
+  const pathGraph = Array.from(pathsMap.values());
   return pathGraph as [PathNode];
 };
 
 export const pathsGraph: [PathNode] = loadPath();
-
-
-
