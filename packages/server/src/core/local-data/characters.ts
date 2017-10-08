275f134a556a09c8e01cf16b8864ec4f84cd3f22
eitanfr <>
2017-10-08 19:53:32 +0300

    batman and iron man

--- packages/server/src/core/local-data/characters.ts
+++ packages/server/src/core/local-data/characters.ts
@@ -3,23 +3,23 @@ import {CharacterData} from '../../types';
 export const PLAYER_CHARACTERS: CharacterData[] = [
   {
     name: 'Steve Rogers',
-    scale: 1.01,
+    scale: 3.01,
     model: '/assets/models/CaptainAmerica.gltf',
   },
   {
     name: 'Anthony Stark',
-    scale: 0.01,
-    model: '/assets/models/ironman/scene.gltf',
+    scale: 1.0,
+    model: '/assets/models/ironman.gltf',
   },
   {
     name: 'Peter Parker',
-    scale: 0.01,
+    scale: 3.01,
     model: '/assets/models/SpiderMan.gltf',
   },
   {
     name: 'Bruce Wayne',
     scale: 0.01,
-    model: '/assets/models/Batman.gltf',
+    model: '/assets/models/batman.gltf',
   },
   {
     name: 'car',
