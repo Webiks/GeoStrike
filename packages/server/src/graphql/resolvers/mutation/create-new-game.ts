5dbba19800f7f834cf6c1a4cee3e76ce5c98b85a
eitanfr <>
2017-11-06 12:19:47 +0200

    remove unwanted code

--- packages/server/src/graphql/resolvers/mutation/create-new-game.ts
+++ packages/server/src/graphql/resolvers/mutation/create-new-game.ts
@@ -9,8 +9,6 @@ export const createNewGame = (rootValue, {character, username, team, isViewer}, 
   }
   else {
     player = games.addRealPlayerToGame(game.gameId, character, username, team);
-    const player2 = games.addRealPlayerToGame(game.gameId, character, username, team);
-    games.playerReady(game.gameId,player2.playerId);
   }
 
   return {
