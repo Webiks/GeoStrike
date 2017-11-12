65ebd8d63f0bd93b26363cea70f7595439270061
tomermoshe <152430tm>
2017-11-12 14:56:21 +0200

    minor tweak

--- packages/client/src/app/game/views/buildings/buildings.component.ts
+++ packages/client/src/app/game/views/buildings/buildings.component.ts
@@ -17,7 +17,7 @@ export class BuildingsComponent implements OnInit {
   Cesium = Cesium;
   showBuildings = false;
 
-  constructor(private buildingsService: BuildingsService, private character: CharacterService, private cd: ChangeDetectorRef) {
+  constructor(buildingsService: BuildingsService, private character: CharacterService, private cd: ChangeDetectorRef) {
     buildingsService.init();
     this.buildings$ = buildingsService.getBuildings();
   }
