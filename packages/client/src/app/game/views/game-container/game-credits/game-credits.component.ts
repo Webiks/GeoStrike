b3c3de491ec25dca67945b74dcfeaadd9feaee2a
katzi0@gmail.com <katzi0@gmail.com>
2018-02-04 15:02:16 +0200

    added game-credis bottom toolbar

---
+++ packages/client/src/app/game/views/game-container/game-credits/game-credits.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatDialogConfig} from '@angular/material';
+import {CreditsDialogComponent} from "../../credits-dialog/credits-dialog.component";
+
+@Component({
+  selector: 'game-credits',
+  templateUrl: './game-credits.component.html',
+  styleUrls: ['./game-credits.component.scss']
+})
+export class GameCreditsComponent implements OnInit {
+
+  constructor(private  dialog: MatDialog) {
+  }
+
+  ngOnInit() {
+  }
+
+  openCredits() {
+    this.dialog.open(CreditsDialogComponent, {
+      // height: '94%',
+      height: '740px',
+      width: '67%',
+      panelClass: 'container-credits'
+    } as MatDialogConfig)
+  }
+
+}
