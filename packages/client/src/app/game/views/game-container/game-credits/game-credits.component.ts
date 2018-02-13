cfe22c3a7ad563192bff4b837bff3ecba342d082
katzi0@gmail.com <katzi0@gmail.com>
2018-02-13 10:56:25 +0200

    fixed remaining issues on PR

--- packages/client/src/app/game/views/game-container/game-credits/game-credits.component.ts
+++ packages/client/src/app/game/views/game-container/game-credits/game-credits.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
 import {MatDialog, MatDialogConfig} from '@angular/material';
 import {CreditsDialogComponent} from "../../credits-dialog/credits-dialog.component";
 
@@ -7,17 +7,33 @@ import {CreditsDialogComponent} from "../../credits-dialog/credits-dialog.compon
   templateUrl: './game-credits.component.html',
   styleUrls: ['./game-credits.component.scss']
 })
-export class GameCreditsComponent {
+export class GameCreditsComponent implements AfterViewInit {
+  private creditsComponentsWidth: number = 0;
+  private isDialogOpen:boolean = false;
+  @ViewChild("creditsContainer") documentRef: ElementRef;
+
+  @HostListener('window:resize') onResize() {
+    if (this.documentRef) {
+      this.creditsComponentsWidth = this.documentRef.nativeElement.clientWidth;
+    }
+  }
 
   constructor(private  dialog: MatDialog) {
   }
 
-  openCredits() {
-    this.dialog.open(CreditsDialogComponent, {
-      height: '78%',
-      width: '67%',
-      panelClass: 'container-credits'
-    } as MatDialogConfig)
+  ngAfterViewInit() {
+    setTimeout(() => this.creditsComponentsWidth = this.documentRef.nativeElement.clientWidth);
   }
 
+  openCredits() {
+    this.isDialogOpen = true;
+    let dialogRef =
+      this.dialog.open(CreditsDialogComponent, {
+        height: '78%',
+        width: '67%',
+        panelClass: 'container-credits'
+      } as MatDialogConfig)
+
+    dialogRef.afterClosed().subscribe(()=> this.isDialogOpen = false);
+  }
 }
