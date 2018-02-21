5ade6781f9e6ff20eabaf46629a5d3c631ac3a04
katzi0@gmail.com <katzi0@gmail.com>
2018-02-21 10:03:34 +0200

    added flighData to types

---
+++ packages/server/src/graphql/resolvers/mutation/toggle-flight-mode.ts
@@ -0,0 +1,35 @@
+import { IGraphQLContext } from '../../context';
+import { ESubscriptionTopics, pubsub } from '../../pubsub';
+import { CharacterType, IPlayer } from '../../../core/local-data/game-manager';
+
+export const notifyKill = (rootValue, { playerId }, { games, game, player }: IGraphQLContext) => {
+  if (!game || !player) {
+    return null;
+  }
+
+  const shootingPlayer = ((game.controlledPlayersMap.get(playerId) || player) as IPlayer);
+  const shotPlayer = game.playersMap.get(playerId);
+  const shotPlayerState = shotPlayer.state;
+
+  games.updatePlayerState(game.gameId, playerId, 'DEAD');
+  pubsub.publish(ESubscriptionTopics.GAME_STATE_CHANGED, { gameData: game });
+
+  if (!shootingPlayer || !shootingPlayer.team || !shotPlayer || !shotPlayer.team ||
+    shotPlayerState === 'DEAD' ||
+    shotPlayer.type === CharacterType.BACKGROUND_CHARACTER) {
+    return shotPlayer;
+  }
+  const killingPlayerTeamColor = shootingPlayer.team.toString().toLowerCase();
+  const killedPlayerTeamColor = shotPlayer.team.toString().toLowerCase();
+
+  let message =
+    `<span style="color:${killingPlayerTeamColor}">${shootingPlayer.username}</span> <span>has killed</span> <span style="color:${killedPlayerTeamColor}">${shotPlayer.username}</span>`;
+  if (playerId === player.playerId){
+    message =
+      `<span style="color: white">${shootingPlayer.username} left the game</span>`;
+  }
+
+  pubsub.publish(ESubscriptionTopics.GAME_NOTIFICATIONS, { gameNotifications: { message, gameId: game.gameId } });
+
+  return shotPlayer;
+};
