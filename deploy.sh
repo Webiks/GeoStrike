6f0eb21a380e4a6140d6c103b9b4675025194240
katzi0@gmail.com <katzi0@gmail.com>
2018-04-08 15:00:53 +0300

    fixed flight swing

--- deploy.sh
+++ deploy.sh
@@ -2,56 +2,50 @@
 
 # --- deploying server ---
 # cd packages/server
-# ../../deploy.sh cesium-server $VERSION $isStage
+# ../../deploy.sh server-[stable or stage] $VERSION
 
 # --- deploying client ---
 # cd packages/client
 # npm run build:prod
-# ../../deploy.sh cesium/client $VERSION $isStage
+# ../../deploy.sh client-[stable or stage] $VERSION
 
 target=$1
 echo "target $target"
 if [ -z "$target" ]
 then
-	echo "Missing deployment target"
+   echo "Missing deployment target"
+else
+   target=geostrike-$target
 fi
 
 version=$2
 echo "version $version"
 if [ -z "$version" ]
 then
-	echo "Missing deployment version"
+   echo "Missing deployment version"
 fi
 
-isStage=$3
-echo "isStage $isStage"
-if [ -z "$isStage" ]
-then
-    isStage=""
-else
-    isStage=-stage
-fi
 
-$SHELL
-exit
+serverAddress=223455578796.dkr.ecr.eu-central-1.amazonaws.com/
 
-echo "start deploying version $version on target $target isStage $isStage"
+uploadServerAddress=$serverAddress$target:$version
+echo "$uploadServerAddress"
 
-eval $(aws ecr get-login --no-include-email --region     eu-central-1)
+
+eval $(aws ecr get-login --no-include-email --region eu-central-1)
 
 docker build -t "$target:$version" .
 
-docker tag "$target$isStage:$version" "$target:latest"
+docker tag "$target:$version" "$target:latest"
 
-docker tag "$target$isStage:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:latest"
+docker tag "$target:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:latest"
 
-docker tag "$target$isStage:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:$version"
+docker tag "$target:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:$version"
 
-docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:latest"
+docker push "2234555787 96.dkr.ecr.eu-central-1.amazonaws.com/$target:latest"
 
-docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:$version"
+docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:$version"
 
 echo "deployment succeeded";
 
-PAUSE
 read -n1 -r -p "Press any key to continue..." key
