e8db0c0010248696ed0ceef975a8fbce56c46312
ohad <master.gefen@gmail.com>
2018-04-10 16:31:46 +0300

    prepare the ground to aws stage

--- deploy.sh
+++ deploy.sh
@@ -2,30 +2,35 @@
 
 # --- deploying server ---
 # cd packages/server
-# ../../deploy.sh cesium-server $VERSION
+# ../../deploy.sh server-[stable or stage] $VERSION
 
 # --- deploying client ---
 # cd packages/client
 # npm run build:prod
-# ../../deploy.sh cesium/client $VERSION
+# ../../deploy.sh client-[stable or stage] $VERSION
 
 target=$1
 echo "target $target"
 if [ -z "$target" ]
 then
-	echo "Missing deployment target"
-	exit 1
+   echo "Missing deployment target"
+else
+   target=geostrike-$target
 fi
 
 version=$2
 echo "version $version"
 if [ -z "$version" ]
 then
-	echo "Missing deployment version"
-	exit 1
+   echo "Missing deployment version"
 fi
 
-echo "start deploying version $version on target $target"
+
+serverAddress=223455578796.dkr.ecr.eu-central-1.amazonaws.com/
+
+uploadServerAddress=$serverAddress$target:$version
+echo "$uploadServerAddress"
+
 
 eval $(aws ecr get-login --no-include-email --region eu-central-1)
 
@@ -37,11 +42,10 @@ docker tag "$target:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$ta
 
 docker tag "$target:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:$version"
 
-docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:latest"
+docker push "2234555787 96.dkr.ecr.eu-central-1.amazonaws.com/$target:latest"
 
 docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:$version"
 
 echo "deployment succeeded";
 
-PAUSE
 read -n1 -r -p "Press any key to continue..." key
