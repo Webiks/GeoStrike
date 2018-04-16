ea2169cdfcaa7ee117b21babc3cf888af815cf40
katzi0@gmail.com <katzi0@gmail.com>
2018-04-16 18:32:24 +0300

    shouldAnimate

--- packages/client/src/app/game/views/game-map/viewer-options/cesium-viewer-options.service.ts
+++ packages/client/src/app/game/views/game-map/viewer-options/cesium-viewer-options.service.ts
@@ -11,6 +11,7 @@ export class CesiumViewerOptionsService {
 
   getViewerOption() {
     return {
+      shouldAnimate: true,
       selectionIndicator: false,
       timeline: false,
       infoBox: false,
