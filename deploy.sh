aee5e9405833e0c0280fc5dac01df0399c1525d1
shai <katzi0@gmail.com>
2018-05-01 16:01:53 +0300

    game-room css fixing

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 
 # --- deploying server ---
 # cd packages/server
-# ../../deploy.sh server-[stable or stage] $VERSION $isStage
+# ../../deploy.sh server-[stable or stage] $VERSION
 
 # --- deploying client ---
 # cd packages/client
 # npm run build:prod
-# ../../deploy.sh client-[stable or stage] $VERSION $isStage
+# ../../deploy.sh client-[stable or stage] $VERSION
 
 target=$1
 echo "target $target"
@@ -16,23 +16,12 @@ then
    echo "Missing deployment target"
 else
    target=geostrike-$target
-	echo "Missing deployment target"
 fi
 
 version=$2
 echo "version $version"
 if [ -z "$version" ]
 then
-	echo "Missing deployment version"
-fi
-
-isStage=$3
-echo "isStage $isStage"
-if [ -z "$isStage" ]
-then
-    isStage=""
-else
-    isStage=-stage
    echo "Missing deployment version"
 fi
 
@@ -42,24 +31,20 @@ serverAddress=223455578796.dkr.ecr.eu-central-1.amazonaws.com/
 uploadServerAddress=$serverAddress$target:$version
 echo "$uploadServerAddress"
 
-$SHELL
-exit
 
-echo "start deploying version $version on target $target isStage $isStage"
-
-eval $(aws ecr get-login --no-include-email --region     eu-central-1)
+eval $(aws ecr get-login --no-include-email --region eu-central-1)
 
 docker build -t "$target:$version" .
 
-docker tag "$target$isStage:$version" "$target:latest"
+docker tag "$target:$version" "$target:latest"
 
-docker tag "$target$isStage:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:latest"
+docker tag "$target:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:latest"
 
-docker tag "$target$isStage:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:$version"
+docker tag "$target:latest" "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:$version"
 
-docker push "2234555787 96.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:latest"
+docker push "2234555787 96.dkr.ecr.eu-central-1.amazonaws.com/$target:latest"
 
-docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target$isStage:$version"
+docker push "223455578796.dkr.ecr.eu-central-1.amazonaws.com/$target:$version"
 
 echo "deployment succeeded";
 
---
+++ packages/client/src/environments/environment.stage.ts
@@ -0,0 +1,31 @@
+console.log("env stage")
+export const environment = {
+  production: true,
+  serverUrl: `35.156.14.39:3001`,
+  wsSchema: 'ws',
+  movement: {
+    runningSpeed: 1.0,
+    walkingSpeed: 0.25,
+    crawlingSpeed: 0.1,
+    flyingLowSpeed: 2.0,
+    flyingHighSpeed: 4.0
+  },
+  createPathMode: false,
+  loadTerrain: true,
+  load3dTiles: true,
+  tiles: {
+    urban_url: 'https://beta.cesium.com/api/assets/1461?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWJmM2MzNS02OWM5LTQ3OWItYjEyYS0xZmNlODM5ZDNkMTYiLCJpZCI6NDQsImFzc2V0cyI6WzE0NjFdLCJpYXQiOjE0OTkyNjQ3NDN9.vuR75SqPDKcggvUrG_vpx0Av02jdiAxnnB1fNf-9f7s',
+    mountain_url: 'https://beta.cesium.com/api/assets/1461?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWJmM2MzNS02OWM5LTQ3OWItYjEyYS0xZmNlODM5ZDNkMTYiLCJpZCI6NDQsImFzc2V0cyI6WzE0NjFdLCJpYXQiOjE0OTkyNjQ3NDN9.vuR75SqPDKcggvUrG_vpx0Av02jdiAxnnB1fNf-9f7s',
+    australia_url: 'https://beta.cesium.com/api/assets/1461?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWJmM2MzNS02OWM5LTQ3OWItYjEyYS0xZmNlODM5ZDNkMTYiLCJpZCI6NDQsImFzc2V0cyI6WzE0NjFdLCJpYXQiOjE0OTkyNjQ3NDN9.vuR75SqPDKcggvUrG_vpx0Av02jdiAxnnB1fNf-9f7s',
+    newzealand_url: 'https://beta.cesium.com/api/assets/1461?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYWJmM2MzNS02OWM5LTQ3OWItYjEyYS0xZmNlODM5ZDNkMTYiLCJpZCI6NDQsImFzc2V0cyI6WzE0NjFdLCJpYXQiOjE0OTkyNjQ3NDN9.vuR75SqPDKcggvUrG_vpx0Av02jdiAxnnB1fNf-9f7s'
+  },
+  terrain: {
+    url: 'https://assets.agi.com/stk-terrain/v1/tilesets/world/tiles',
+  },
+  controls: {
+    mouseSensitivity: 16, // bigger is less sensitive
+    disableBackward: true,
+    disableLeft: true,
+    disableRight: true,
+  }
+};
